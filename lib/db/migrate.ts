4dd1b29f7115360228633fc8511a94b5e8266ad2
poteto <naonyan1135@gmail.com>
2025-11-14 01:59:15 +0900

    a

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -2,7 +2,7 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-const sqlite = new Database("./data/gakushukun.db");
+const sqlite = new Database("./data/gakushukun.db",{create:true});
 const db = drizzle(sqlite);
 
 async function runMigration() {
